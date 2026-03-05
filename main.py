1a55bd5dd076dc4c8acb524ed3cbde54f59fe821
Sristy Gupta <sristyvidhyarthi@gmail.com>
2026-03-05 20:51:07 +0530

    add CORS middleware

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 app = FastAPI()
+
+# Add this block ↓
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # --- Step A: Define what the incoming data looks like ---
 class SentimentRequest(BaseModel):
